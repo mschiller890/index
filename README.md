040cf6a0ccf4e7e436cb5744cf3115252824f9f7
Michal Schiller <mschiller890@gmail.com>
2026-07-18 15:47:31 +0200

    Template upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+@@ -1,2 +1,9 @@
 # index
 A powerful chest indexing and item locator mod.
---
+++ src/client/java/com/mschiller890/index/client/IndexClient.java
@@ -0,0 +1,10 @@
+package com.mschiller890.index.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IndexClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
